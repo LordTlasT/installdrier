7b9402a76999635cc8c434b057b610316131b20b
Raymaekers Luca <raymaekers.luca@gmail.com>
2023-01-06 22:23:49 +0100

    vim -> neovim

--- pacinstaller.sh
+++ pacinstaller.sh
@@ -19,6 +19,7 @@ pacman --noconfirm -Syu \
   # npm \
   # jre \
 pacman --noconfirm -S \
+	neovim \
   lynx nnn python-pygments tmux \
   man-db man-pages bash-completion exa \
   htop \
@@ -89,24 +90,23 @@ chsh -s /bin/zsh aluc
 
 pacman --noconfirm -S xorg-server xorg-xinit
 
-git clone https://github.com/vim/vim
-cd vim
-
-pacman --noconfirm -S cmake
-# Bug fix for YouCompleteMe plugin
-export LDFLAGS="-rdynamic"
-./configure \
-  --with-features=huge \
-  --enable-multibyte \
-  --enable-python3interp=yes \
-  --with-python3-config-dir=/usr/lib/python3.10/config-3.10-x86_64-linux-gnu \
-  --enable-gui=gtk2 \
-  --enable-cscope \
-  --prefix=/usr/local
-make
-make install || exit
-cd
-rm -rf vim
+# git clone https://github.com/vim/vim
+# cd vim
+# pacman --noconfirm -S cmake
+# # Bug fix for YouCompleteMe plugin
+# export LDFLAGS="-rdynamic"
+# ./configure \
+#   --with-features=huge \
+#   --enable-multibyte \
+#   --enable-python3interp=yes \
+#   --with-python3-config-dir=/usr/lib/python3.10/config-3.10-x86_64-linux-gnu \
+#   --enable-gui=gtk2 \
+#   --enable-cscope \
+#   --prefix=/usr/local
+# make
+# make install || exit
+# cd
+# rm -rf vim
 
 echo "Don't forget to set a password for '${user}'"
 echo "Don't forget to edit visudo file"
