25e83bd98d7db62638fc26186bb7b9bc5bc5093a
Raymaekers Luca <raymaekers.luca@gmail.com>
2023-01-09 13:58:02 +0100

    added required dummy session for plugins and log

--- arch/tmux.sh
+++ arch/tmux.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env sh
+echo
+echo "I: Starting tmux install script"
 pacman -S --noconfirm \
 	tmux
 
+name="$(date +%H%M%S)-dummy"
+
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+echo "I: Creating dummy session"
+tmux -d -s "$name"
+echo "I: reloading config"
 tmux source ~/.tmux.conf
+echo "I: installing plugins"
 ~/.tmux/plugins/tpm/scripts/install_plugins.sh
+echo "I: Killing dummy session"
+tmux kill-session -t "$name"
+echo "I: Done."
