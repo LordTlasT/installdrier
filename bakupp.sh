86e5591147fc2321313fced66ba8538454f20a29
Raymaekers Luca <raymaekers.luca@gmail.com>
2023-01-16 19:19:00 +0100

    renamed cpdir to cpfiles

--- bakupp.sh
+++ bakupp.sh
@@ -5,7 +5,7 @@ bkpd="$(date +%y%m%d)_bakupp"
 mkdir -p "${bkpd}"
 
 # $1: root dir - $2: files
-function cpdir {
+function cpfiles {
 	mkdir -p "${bkpd}/${1:1}"
 	for f in $2
 	do
@@ -13,6 +13,6 @@ function cpdir {
 	done
 }
 
-cpdir /etc "environment doas.conf hosts pacman.conf default/grub mkinitcpio.conf X11/xorg.conf security/faillock.conf pers.map.gz"
-cpdir ~ "docs music pictures movies zot"
-cpdir ~/.local/share/multimc/ "common"
+cpfiles /etc "environment doas.conf hosts pacman.conf default/grub mkinitcpio.conf X11/xorg.conf security/faillock.conf pers.map.gz"
+cpfiles ~ "docs music pictures movies zot"
+cpfiles ~/.local/share/multimc/ "common"
