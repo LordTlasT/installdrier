ce21fdffdbf505d72e2d2facace1eea7fe169ab0
Raymaekers Luca <raymaekers.luca@gmail.com>
2023-01-22 02:05:32 +0100

    updated nvim dependencies

--- arch/nvim.sh
+++ arch/nvim.sh
@@ -1,13 +1,15 @@
 pacman --noconfirm -S \
 	neovim \ 
-	ripgrep
 
 # Dependencies
 #packer
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 #lua lsp
-pacman --noconfirm -S lua-language-server
+pacman --noconfirm -S lua-language-server \
+	npm \
+	ripgrep
+npm -g live-server
 
 #install plugins
 nvim -c 'PackerSync'
