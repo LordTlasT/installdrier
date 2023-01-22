19c50ffad57b0d894eac6ee2642e0f942114ca5e
Raymaekers Luca <raymaekers.luca@gmail.com>
2023-01-22 03:09:30 +0100

    updated Dependencies

--- arch/shell.sh
+++ arch/shell.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
+pacman --noconfirm  zsh 
 
+# Dependencies
 pacman --noconfirm -S \
   man-db man-pages bash-completion exa
-
+# zsh
 pacman --noconfirm -S \
- zsh zsh-syntax-highlighting zsh-autosuggestions starship \
+ zsh-syntax-highlighting zsh-autosuggestions starship
+curl -L git.io/antigen > ~/.zsh/antigen.zsh
+
   || exit 1
 chsh -s /bin/zsh ${user}
