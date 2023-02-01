909ee2c90e02bf4c58bb56034877102fa6b3269a
Raymaekers Luca <raymaekers.luca@gmail.com>
2023-02-01 11:32:58 +0100

    added: (nvim) arduino-language-server

--- arch/nvim.sh
+++ arch/nvim.sh
@@ -8,12 +8,14 @@ git clone --depth 1 https://github.com/wbthomason/packer.nvim\
 #lua lsp
 pacman --noconfirm -S lua-language-server \
 	npm \
+	go \
 	ripgrep
 npm i -g live-server
 #lsps
 npm i -g unocss-language-server 
 npm i -g typescript typescript-language-server
 npm i -g emmet-ls
+go install github.com/arduino/arduino-language-server@latest
 
 #install plugins
 nvim -c 'PackerSync'
