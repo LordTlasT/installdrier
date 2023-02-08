5c2e26c0156df01239743a4aee789b1a2355f413
Raymaekers Luca <raymaekers.luca@gmail.com>
2023-02-08 18:30:38 +0100

    added: (arch audio) option for pipewire

--- arch/audio.sh
+++ arch/audio.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
-
-pacman --noconfirm -S \
-	pulseaudio pulseaudio-alsa pulsemixer pamixer \
-	spotifyd
+case "${IND_SERVER:-pulse}" in
+	"pulse")
+		pacman --noconfirm -S \
+			pulseaudio pulseaudio-alsa pulsemixer pamixer \
+			spotifyd
+		;;
+	"pipewire")
+		pacman --noconfirm -S \
+			pipewire wireplumber \
+			pipewire-pulse pipewire-alsa
+		# noisetorch ? easyeffects?
+		;;
+	*)
+		echo "no server?"
+esac
