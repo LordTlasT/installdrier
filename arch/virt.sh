4af244680f24417bee598e396b133ef4ad006616
Raymaekers Luca <raymaekers.luca@gmail.com>
2023-03-30 11:13:11 +0200

    added default user for virt.sh

--- arch/virt.sh
+++ arch/virt.sh
@@ -2,14 +2,14 @@
 
 pacman --noconfirm -S \
     virtualbox virtualbox-guest-iso
-usermod -aG vboxusers "${user}"
+usermod -aG vboxusers "${user:=aluc}"
 modprobe vboxdrv vboxnetadp vboxnetflt
 
 pacman --noconfirm -S \
-virt-manager qemu-desktop libvirt edk2-ovmf dnsmasq iptables-nft
-sudo usermod -aG libvirt "${user}"
+	virt-manager qemu-desktop libvirt edk2-ovmf dnsmasq iptables-nft
+sudo usermod -aG libvirt "${user:=aluc}"
 echo "unix_sock_rw_perms = '0770'" | sudo tee -a /etc/libvirt/libvirtd.conf
 echo "unix_sock_group = 'libvirt'" | sudo tee -a /etc/libvirt/libvirtd.conf
-echo "user = ${user}"| sudo tee -a /etc/libvirt/qemu.conf
-echo "group = ${user}" | sudo tee -a /etc/libvirt/qemu.conf
+echo "user = ${user:=aluc}" | sudo tee -a /etc/libvirt/qemu.conf
+echo "group = ${user:=aluc}" | sudo tee -a /etc/libvirt/qemu.conf
 virsh pool-define-as VMs dir - - - - /home/aluc/Media/sdb/VMs
