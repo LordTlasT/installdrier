37087650ac09767c39c7682a1d986eb23bcb709b
Raymaekers Luca <raymaekers.luca@gmail.com>
2023-04-09 22:38:16 +0200

    cleanup of nvim for usage as user

--- arch/scripts/nvim.sh
+++ arch/scripts/nvim.sh
@@ -1,24 +1,41 @@
-pacman --noconfirm -S neovim
-ln -sf /usr/bin/nvim /usr/bin/vim
+#!/bin/sh
+
+die () {
+	echo "$1" >&2
+}
+
+die "installing neovim"
+doas pacman --noconfirm -S neovim > /dev/null 2>&1
+die "symlinking vim"
+doas ln -sf /usr/bin/nvim /usr/bin/vim
 
 # Dependencies
 #packer
+
+die "installing packer"
 git clone --depth 1 https://github.com/wbthomason/packer.nvim \
- $HOME/.local/share/nvim/site/pack/packer/start/packer.nvim
+ $HOME/.local/share/nvim/site/pack/packer/start/packer.nvim > /dev/null 2>&1
+
+die "installing dependencies with pacman"
 #lua lsp
-pacman --noconfirm -S \
+doas pacman --noconfirm -S \
 	lua-language-server \
+	jedi-language-server \
 	npm \
 	go \
-	ripgrep
+	ripgrep 2>&1 > /dev/null
 
-npm i -g live-server
+die "installing live server"
+npm i -g live-server > /dev/null 2>&1
 #lsps
-npm i -g unocss-language-server 
-npm i -g typescript typescript-language-server
-npm i -g emmet-ls
-go install github.com/arduino/arduino-language-server@latest
+die "unocss lsp"
+npm i -g unocss-language-server  > /dev/null 2>&1
+die "js lsp"
+npm i -g typescript typescript-language-server > /dev/null 2>&1
+die "emmet"
+npm i -g emmet-ls > /dev/null 2>&1
+die "arduino lsp"
+go install github.com/arduino/arduino-language-server@latest > /dev/null 2>&1
 
 #install plugins
 nvim -c 'PackerSync'
-pacman -S --noconfirm jedi-language-server
