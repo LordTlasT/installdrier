c41f0532b248e4d0673d44ab1e41f4b22419ab96
Raymaekers Luca <raymaekers.luca@gmail.com>
2023-04-10 14:26:01 +0200

    added case to virt.sh

--- arch/scripts/virt.sh
+++ arch/scripts/virt.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env sh
 
-pacman --noconfirm -S \
-    virtualbox virtualbox-guest-iso
-usermod -aG vboxusers "${user:=aluc}"
-modprobe vboxdrv vboxnetadp vboxnetflt
-
-pacman --noconfirm -S \
-	virt-manager qemu-desktop libvirt edk2-ovmf dnsmasq iptables-nft
-sudo usermod -aG libvirt "${user:=aluc}"
-echo "unix_sock_rw_perms = '0770'" | sudo tee -a /etc/libvirt/libvirtd.conf
-echo "unix_sock_group = 'libvirt'" | sudo tee -a /etc/libvirt/libvirtd.conf
-echo "user = ${user:=aluc}" | sudo tee -a /etc/libvirt/qemu.conf
-echo "group = ${user:=aluc}" | sudo tee -a /etc/libvirt/qemu.conf
-virsh pool-define-as VMs dir - - - - /home/aluc/Media/sdb/VMs
+case ${1:-virtualbox} in
+	"virtualbox")
+		pacman --noconfirm -S \
+			virtualbox virtualbox-guest-iso \
+			2>&1 | grep "installing"
+		usermod -aG vboxusers "${user:=aluc}"
+		modprobe vboxdrv vboxnetadp vboxnetflt
+		;;
+	"virtmanager")
+		pacman --noconfirm -S \
+			virt-manager qemu-desktop libvirt edk2-ovmf dnsmasq iptables-nft \
+			2>&1 | grep "installing"
+		sudo usermod -aG libvirt "${user:=aluc}"
+		echo "unix_sock_rw_perms = '0770'" | sudo tee -a /etc/libvirt/libvirtd.conf
+		echo "unix_sock_group = 'libvirt'" | sudo tee -a /etc/libvirt/libvirtd.conf
+		echo "user = ${user:=aluc}" | sudo tee -a /etc/libvirt/qemu.conf
+		echo "group = ${user:=aluc}" | sudo tee -a /etc/libvirt/qemu.conf
+		;;
+esac
