060fa313e80cbe990480db925b55b762b18f218a
Raymaekers Luca <raymaekers.luca@gmail.com>
2023-04-11 11:31:54 +0200

    added cgit.sh

---
+++ deb/scripts/cgit.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+apt-get install -y libzip-dev libssl-dev libcrypt-dev fcgiwrap
+git clone --depth 1 https://git.zx2c4.com/cgit/ ||
+	exit 1
+cd cgit
+git submodule init && git submodule update && make ||
+	exit 1
+make install
