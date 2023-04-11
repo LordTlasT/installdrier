60cc5bb6395f1d733a6668677e921a3bcaacc9e4
Raymaekers Luca <raymaekers.luca@gmail.com>
2023-04-11 18:12:06 +0200

    added creation of /srv dir

--- deb/scripts/cgit.sh
+++ deb/scripts/cgit.sh
@@ -6,3 +6,8 @@ cd cgit
 git submodule init && git submodule update && make ||
 	exit 1
 make install
+
+mkdir -p /srv/cgit/repos /srv/cgit/conf
+cp cgit.css cgit.png robots.txt favicon.ico /srv/cgit/conf
+
+chown -R www-data:www-data /srv/cgit
