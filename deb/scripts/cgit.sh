2af3f6225b78372dd1264cdd48789d263fbc05df
Raymaekers Luca <raymaekers.luca@gmail.com>
2023-04-11 21:07:59 +0200

    copy htdcos instead of creating a directory

--- deb/scripts/cgit.sh
+++ deb/scripts/cgit.sh
@@ -8,6 +8,6 @@ git submodule init && git submodule update && make ||
 make install
 
 mkdir -p /srv/cgit/repos /srv/cgit/conf
-cp cgit.css cgit.png robots.txt favicon.ico /srv/cgit/conf
+cp -r /var/www/htdocs/cgit /srv/cgit/conf
 
 chown -R www-data:www-data /srv/cgit
